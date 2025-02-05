1a83b4e57f6c91cbebaf5538e7fbffb602e3a49b
CuriousGeorg1 <georg.m.wahlroos@gmail.com>
2025-02-05 12:38:21 +0200

    buttons for feedback and rendering

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -6,7 +6,63 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const TotalFeedback = () => {
+    return <p>Total feedback: {good + neutral + bad}</p>;
+  };
+
+  const AverageScore = () => {
+    if (good + neutral + bad === 0) {
+      return <p>Average score: 0</p>;
+    }
+    return <p>Average score: {(good - bad) / (good + neutral + bad)}</p>;
+  };
+
+  const PositivePercentage = () => {
+    if (good + neutral + bad === 0) {
+      return <p>Positive percentage: Not enough data</p>;
+    }
+    return <p>Positive percentage: {(good / (good + neutral + bad)) * 100}%</p>;
+  };
+
+  const Good = () => {
+    return <p>Good: {good}</p>;
+  };
+
+  const Neutral = () => {
+    return <p>Neutral: {neutral}</p>;
+  };
+
+  const Bad = () => {
+    return <p>Bad: {bad}</p>;
+  };
+
+  const Statistics = () => {
+    return (
+      <div>
+        <h1>Give feedback</h1>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+        <Good />
+        <Neutral />
+        <Bad />
+
+        <div>
+          <h1>Statistics</h1>
+          <TotalFeedback />
+          <AverageScore />
+          <PositivePercentage />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Statistics />
+    </div>
+  );
 };
 
 export default App;
