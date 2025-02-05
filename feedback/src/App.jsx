4bb21d0a0b37a47a0d700c641304015335331ed8
CuriousGeorg1 <georg.m.wahlroos@gmail.com>
2025-02-05 12:57:56 +0200

    more cleaning

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -1,46 +1,46 @@
 import { useState } from "react";
 
+const TotalFeedback = ({ good, neutral, bad }) => {
+  return <p>Total feedback: {good + neutral + bad}</p>;
+};
+
+const AverageScore = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return <p>Average score: 0</p>;
+  }
+  return <p>Average score: {(good - bad) / (good + neutral + bad)}</p>;
+};
+
+const PositivePercentage = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return <p>Positive percentage: Not enough data</p>;
+  }
+  return <p>Positive percentage: {(good / (good + neutral + bad)) * 100}%</p>;
+};
+
+const FeedbackCount = ({ label, count }) => {
+  return (
+    <p>
+      {label}: {count}
+    </p>
+  );
+};
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <TotalFeedback good={good} neutral={neutral} bad={bad} />
+      <AverageScore good={good} neutral={neutral} bad={bad} />
+      <PositivePercentage good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const TotalFeedback = ({ good, neutral, bad }) => {
-    return <p>Total feedback: {good + neutral + bad}</p>;
-  };
-
-  const AverageScore = ({ good, neutral, bad }) => {
-    if (good + neutral + bad === 0) {
-      return <p>Average score: 0</p>;
-    }
-    return <p>Average score: {(good - bad) / (good + neutral + bad)}</p>;
-  };
-
-  const PositivePercentage = ({ good, neutral, bad }) => {
-    if (good + neutral + bad === 0) {
-      return <p>Positive percentage: Not enough data</p>;
-    }
-    return <p>Positive percentage: {(good / (good + neutral + bad)) * 100}%</p>;
-  };
-
-  const FeedbackCount = ({ label, count }) => {
-    return (
-      <p>
-        {label}: {count}
-      </p>
-    );
-  };
-  const Statistics = ({ good, neutral, bad }) => {
-    return (
-      <div>
-        <h1>Statistics</h1>
-        <TotalFeedback good={good} neutral={neutral} bad={bad} />
-        <AverageScore good={good} neutral={neutral} bad={bad} />
-        <PositivePercentage good={good} neutral={neutral} bad={bad} />
-      </div>
-    );
-  };
 
   return (
     <div>
